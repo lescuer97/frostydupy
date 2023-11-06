deda503c79a9591836c0e406d00a8da4fb495d4d
leonardo <leoescuer@protonmail.com>
2023-11-06 19:19:17 +0100

    workspace and bdk alpha setup

---
+++ gui/src/main.rs
@@ -0,0 +1,12 @@
+slint::slint!{
+    export component HelloWorld {
+        Text {
+            text: "hello world";
+            color: green;
+        }
+    }
+}
+
+fn main() {
+    HelloWorld::new().unwrap().run().unwrap();
+}
---
+++ wallet/src/lib.rs
@@ -0,0 +1,24 @@
+use serde::{Deserialize, Serialize};
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+ 
+pub fn create_multisig_wallet(sig1: String, sig2: String, sig3: String) -> Wallet {
+    // Wallet {
+    //     id: 1,
+    //     name: "My Wallet".to_string(),
+    //     balance: 0,
+    // }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
